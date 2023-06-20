b23198a39386619852357ec7637479f78ab52063
hassan <hchoudhury20@hotmail.com>
2023-06-20 15:14:50 +0100

    allow users to dynamically make api requests

--- weather.js
+++ weather.js
@@ -1,4 +1,35 @@
-fetch("https://api.weatherapi.com/v1/current.json?key=5999c6f404a84d23930115224231906&q=london",{mode:"cors"})
-.then(function(response){
-    console.log(response.json());
-});
+const button = document.querySelector("button");
+const area = document.querySelector("#location")
+
+function request(yee){
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=5999c6f404a84d23930115224231906&q=${yee}`,{mode:"cors"})
+    .then(function(response){
+        console.log(response.json());
+   });
+  }
+
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    input = area.value
+    request(input)
+   
+
+  
+   
+  });
+
+
+
+  function request(yee){
+
+    fetch(`https://api.weatherapi.com/v1/current.json?key=5999c6f404a84d23930115224231906&q=${yee}`,{mode:"cors"})
+    .then(function(response){
+        console.log(response.json());
+   });
+  }
+
+
+
+
+  
